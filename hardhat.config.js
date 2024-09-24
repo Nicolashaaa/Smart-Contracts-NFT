5a6ac587d148b4c7e0a2243611c523052b2f97e3
Nicola <fuckedupupd@DESKTOP-CHULFK6.localdomain>
2024-09-25 02:11:04 +0300

    created NFT and MARKET smart contracts, added tests and deployment script

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+/* hardhat.config.js */
+const { hardHatSettings } = require("./scripts/helpers.js");
+
+require("@nomiclabs/hardhat-waffle");
+require("./scripts/deploy.js");
+
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  defaultNetwork: "hardhat",
+  networks: hardHatSettings.networks,
+}
